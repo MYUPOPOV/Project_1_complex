dc9216671589d249269515f57fe008918cfc3ad0
MYuPopov <beemax@bk.ru>
2021-11-26 17:22:30 +0300

    Исправления после проверки куратором

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let strArray = num.toString().split("");
 let sum =
 	strArray.reduce((sum, current) => {
 		return sum * current;
-	}, 1) ** 2; // jshint ignore:line
+	}, 1) ** 3; // jshint ignore:line
 
 console.log(sum);
 console.log(sum.toString().slice(0, 2));
