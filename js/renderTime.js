29678b94c43901287de780012dcc421bf5160c10
MYuPopov <beemax@bk.ru>
2021-12-22 14:43:01 +0300

    Обязатльное задание урока 19

--- js/renderTime.js
+++ js/renderTime.js
@@ -1,9 +1,11 @@
-/* Урок 8. Выводит текущей даты и времени */
+/* Урок 19. Вывод текущей даты и времени */
 let Now;
 const stringA = document.querySelector(".string_a");
 const stringB = document.querySelector(".string_b");
+const stringC = document.querySelector(".string_c");
+const stringD = document.querySelector(".string_d");
 
-/* Склоняем слово "час" */
+/* Склоняем слова: "час" / "минута" и "секунда" */
 const hourDeclension = (hour) => {
 	if ((hour >= 2 && hour <= 4) || (hour >= 22 && hour <= 23)) {
 		return "часа";
@@ -15,61 +17,18 @@ const hourDeclension = (hour) => {
 		return "час";
 	}
 };
-
-/* Склоняем слово "минута" */
-const minutesDeclension = (minutes) => {
-	if (minutes === 1 || minutes === 21 || minutes === 31 || minutes === 41 || minutes === 51) {
-		return "минута";
+const declensionItems = (item, word) => {
+	if (item === 1 || item === 21 || item === 31 || item === 41 || item === 51) {
+		return word + "а";
 	}
-	if (
-		(minutes >= 2 && minutes <= 4) ||
-		(minutes >= 22 && minutes <= 24) ||
-		(minutes >= 32 && minutes <= 34) ||
-		(minutes >= 42 && minutes <= 44) ||
-		(minutes >= 52 && minutes <= 54)
-	) {
-		return "минуты";
+	if ((item >= 2 && item <= 4) || (item >= 22 && item <= 24) || (item >= 32 && item <= 34) || (item >= 42 && item <= 44) || (item >= 52 && item <= 54)) {
+		return word + "ы";
 	}
 
-	if (
-		minutes === 0 ||
-		(minutes >= 5 && minutes <= 20) ||
-		(minutes >= 25 && minutes <= 30) ||
-		(minutes >= 35 && minutes <= 40) ||
-		(minutes >= 45 && minutes <= 50) ||
-		(minutes >= 55 && minutes <= 60)
-	) {
-		return "минут";
+	if (item === 0 || (item >= 5 && item <= 20) || (item >= 25 && item <= 30) || (item >= 35 && item <= 40) || (item >= 45 && item <= 50) || (item >= 55 && item <= 60)) {
+		return word;
 	}
 };
-/* Склоняем слово "минута" ~ */
-
-/* Склоняем слово "секунда" */
-const secundsDeclension = (secunds) => {
-	if (secunds === 1 || secunds === 21 || secunds === 31 || secunds === 41 || secunds === 51) {
-		return "секунда";
-	}
-	if (
-		(secunds >= 2 && secunds <= 4) ||
-		(secunds >= 22 && secunds <= 24) ||
-		(secunds >= 32 && secunds <= 34) ||
-		(secunds >= 42 && secunds <= 44) ||
-		(secunds >= 52 && secunds <= 54)
-	) {
-		return "секунды";
-	}
-	if (
-		secunds === 0 ||
-		(secunds >= 5 && secunds <= 20) ||
-		(secunds >= 25 && secunds <= 30) ||
-		(secunds >= 35 && secunds <= 40) ||
-		(secunds >= 45 && secunds <= 50) ||
-		(secunds >= 55 && secunds <= 60)
-	) {
-		return "секунд";
-	}
-};
-/* Склоняем слово "секунда" ~ */
 
 /* Форматируем отображение духзначных чисел */
 const timeFormat = (value) => {
@@ -85,137 +44,68 @@ const getTime = () => {
 	const date = new Date();
 	// const date = new Date(2021, 11, 7, 13, 21, 5);
 
-	// В переменную dayOfWeek записываем день недели
-	let dayOfWeek;
-	switch (date.getDay()) {
-		case 0:
-			dayOfWeek = "Воскресенье";
-			break;
-		case 1:
-			dayOfWeek = "Понедельник";
-			break;
-		case 2:
-			dayOfWeek = "Вторник";
-			break;
-		case 3:
-			dayOfWeek = "Среда";
-			break;
-		case 4:
-			dayOfWeek = "Четверг";
-			break;
-		case 5:
-			dayOfWeek = "Пятница";
-			break;
-		case 6:
-			dayOfWeek = "Суббота";
-			break;
-	}
-	// console.log("~ dayOfWeek:", dayOfWeek);
-	// В переменную dayOfWeek записываем день недели ~
-
+	const week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+	let dayOfWeek = week[date.getDay()]; // день недели
 	const dayOfMonth = date.getDate(); // сегодняшнее число
-	// console.log("~ dayOfMonth", dayOfMonth);
-
-	// В переменную month записываем текущий месяц
-	let month;
-	switch (date.getMonth()) {
-		case 0:
-			month = "января";
-			break;
-		case 1:
-			month = "февраля";
-			break;
-		case 2:
-			month = "марта";
-			break;
-		case 3:
-			month = "апреля";
-			break;
-		case 4:
-			month = "мая";
-			break;
-		case 5:
-			month = "июня";
-			break;
-		case 6:
-			month = "июля";
-			break;
-		case 7:
-			month = "августа";
-			break;
-		case 8:
-			month = "сентября";
-			break;
-		case 9:
-			month = "октября";
-			break;
-		case 10:
-			month = "ноября";
-			break;
-		case 11:
-			month = "декабря";
-			break;
-	}
-	// console.log("~ month", month);
-	// В переменную month записываем текущий месяц ~
-
-	const year = date.getFullYear(); // сегодняшнее число
-	// console.log("~ year", year);
-
+	const months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+	let month = months[date.getMonth()]; // текущий месяц
+	const year = date.getFullYear(); // сегодняшний год
 	const hour = date.getHours(); // текущий час
-	const hourDecl = hourDeclension(hour);
-	// console.log("~ hour", hour);
-	// console.log("~ hourDeclension", hourDecl);
-
 	const minutes = date.getMinutes(); // текущая минута
-	// console.log("~ minutes", minutes);
-	const minutesDecl = minutesDeclension(minutes);
-	// console.log("~ minutesDecl", minutesDecl);
-
 	const secunds = date.getSeconds(); // текущая минута
-	// console.log("~ secunds", secunds);
-	const secundsDecl = secundsDeclension(secunds);
-	// console.log("~ secundsDecl", secundsDecl);
-
-	// Отформатированные переменные для string2
+	/* 	Склоняем часы, минуты, секунды:  */
+	const hourDecl = hourDeclension(hour);
+	const minutesDecl = declensionItems(minutes, "минут");
+	const secundsDecl = declensionItems(secunds, "секунд");
+	/* 	Отформатированные переменные:  */
 	const dayOfMonthFormat = timeFormat(dayOfMonth);
 	const monthFormat = timeFormat(date.getMonth() + 1);
-	const hourFormat = timeFormat(hour);
+	let hourFormat = timeFormat(hour);
 	const minutesFormat = timeFormat(minutes);
 	const secundsFormat = timeFormat(secunds);
 
-	const string1 =
-		"Сегодня " +
-		dayOfWeek +
-		", " +
-		dayOfMonth +
-		" " +
-		month +
-		" " +
-		year +
-		" года, " +
-		hour +
-		" " +
-		hourDecl +
-		" " +
-		minutes +
-		" " +
-		minutesDecl +
-		" " +
-		secunds +
-		" " +
-		secundsDecl;
-	const string2 = dayOfMonthFormat + "." + monthFormat + "." + year + " - " + hourFormat + ":" + minutesFormat + ":" + secundsFormat;
+	let string1; // Первая строка (время дня)
+	if (hour >= 4 && hour < 12) {
+		string1 = "Доброе утро!";
+	} else if (hour >= 12 && hour < 17) {
+		string1 = "Добрый день!";
+	} else if (hour >= 17 && hour < 24) {
+		string1 = "Добрый вечер!";
+	} else if (hour >= 0 && hour < 4) {
+		string1 = "Доброй ночи!";
+	}
 
-	return [string1, string2];
+	const string2 = `Сегодня: ${dayOfWeek}, ${dayOfMonth} ${month} ${year} года`; // День недели и дата
+	let timeId = "AM";
+	if (hour > 12) {
+		timeId = "PM";
+		hourFormat = hour - 12;
+	}
+
+	const string3 = `Текущее время: ${hourFormat}:${minutesFormat}:${secundsFormat} ${timeId} (${hour} ${hourDecl} ${minutes} ${minutesDecl} и ${secunds} ${secundsDecl})`; // Время
+
+	// let dateStop = new Date(deadline).getTime();
+	// let dateNow = new Date().getTime();
+	// let timeRemaining = (dateStop - dateNow) / 1000;
+	// let days = Math.floor(timeRemaining / 3600 / 24);
+
+	// ${}
+
+	// const string1 = "Сегодня " + dayOfWeek + ", " + dayOfMonth + " " + month + " " + year + " года, " + hour + " " + hourDecl + " " + minutes + " " + minutesDecl + " " + secunds + " " + secundsDecl;
+	// const string2 = dayOfMonthFormat + "." + monthFormat + "." + year + " - " + hourFormat + ":" + minutesFormat + ":" + secundsFormat;
+
+	// return [string1, string2, string3, string4];
+	return [string1, string2, string3];
 };
 
 /* Отрисовываем время */
 const renderTime = () => {
 	getTime();
-	Now = getTime();
-	stringA.innerHTML = "<i>" + Now[0] + "</i>";
-	// stringB.innerHTML = Now[1];
+	timeArray = getTime();
+	stringA.innerHTML = "<i>" + timeArray[0] + "</i>";
+	stringB.innerHTML = "<i>" + timeArray[1] + "</i>";
+	stringC.innerHTML = "<i>" + timeArray[2] + "</i>";
+	// stringA.innerHTML = "<i>" + timeArray[3] + "</i>";
 };
 
 // Запускаем :)
